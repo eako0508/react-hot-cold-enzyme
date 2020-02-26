74d82a906e6f07cc29574569c2c9f3ee75335b2c
Eugene <eako0508@gmail.com>
2020-02-26 12:32:20 -0500

    guess-form test complete

--- src/components/__tests__/guess-form.test.js
+++ src/components/__tests__/guess-form.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-
+import {shallow, mount} from 'enzyme';
+ 
 import GuessForm from './../guess-form';
 
 describe('<GuessForm />', () =>{
 	it('Renders without crashing', ()=>{
 		shallow(<GuessForm />);
+	});
+
+	it('User\'s input uploads to onMakeGuess prop ', ()=>{		
+		const callback = jest.fn();
+		const wrapper = mount(<GuessForm onMakeGuess={callback}/>);
+		const expectedResult = "50";
+
+		wrapper.find('#userGuess').instance().value = expectedResult;	
+		wrapper.simulate('submit', {preventDefault(){}});
+
+		expect(callback).toHaveBeenCalledWith(expectedResult);
 	});
 });
