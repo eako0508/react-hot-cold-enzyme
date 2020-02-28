816771c07188f57b4698fef45ab6fef54c6b9246
Eugene <eako0508@gmail.com>
2020-02-28 10:55:40 -0500

    guessList Complete

--- src/components/__tests__/guess-list.test.js
+++ src/components/__tests__/guess-list.test.js
@@ -6,5 +6,20 @@ import GuessList from './../guess-list';
 describe('<GuessList />', () =>{
 	it('Renders without crashing', ()=>{
 		shallow(<GuessList guesses={[]}/>);
+		const wrapper = shallow(<GuessList guesses={[]}/>);
+		
+		expect(wrapper)
+	});
+
+	it('Rander guesses', ()=>{
+		const guessArray = [1,2,3];
+		const wrapper = shallow(<GuessList guesses={guessArray}/>);
+		expect(wrapper.exists('li')).toBe(true);
+	});
+
+	it('Rander 3 guesses', ()=>{
+		const guessArray = [1,2,3];
+		const wrapper = shallow(<GuessList guesses={guessArray}/>);
+		expect(wrapper.find('li')).toHaveLength(3);
 	});
 });
