5645663c5a56bff00704c92dee3743b9d4835698
eako0508 <eako0508@gmail.com>
2020-02-28 11:34:12 -0500

    test/guess-count complete

--- src/components/__tests__/guess-count.test.js
+++ src/components/__tests__/guess-count.test.js
@@ -5,6 +5,12 @@ import GuessCount from './../guess-count';
 
 describe('<GuessCount />', () =>{
 	it('Renders without crashing', ()=>{
-		shallow(<GuessCount />);
+		shallow(<GuessCount guessCount={0}/>);
+	});
+
+	it('Displays number correctly', ()=>{
+		const guessLength = 5;
+		const wrapper = shallow(<GuessCount guessCount={guessLength}/>);
+		expect(wrapper.find('#count').text()).toEqual(guessLength.toString());
 	});
 });
