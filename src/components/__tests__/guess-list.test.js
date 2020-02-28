d532d507333e3783d48fa9ba1f2905bafb7af0a9
eako0508 <eako0508@gmail.com>
2020-02-28 11:23:35 -0500

    test/guess-list: added test for each elements

--- src/components/__tests__/guess-list.test.js
+++ src/components/__tests__/guess-list.test.js
@@ -6,20 +6,31 @@ import GuessList from './../guess-list';
 describe('<GuessList />', () =>{
 	it('Renders without crashing', ()=>{
 		shallow(<GuessList guesses={[]}/>);
-		const wrapper = shallow(<GuessList guesses={[]}/>);
+		//const wrapper = shallow(<GuessList guesses={[]}/>);
 		
-		expect(wrapper)
+		//expect(wrapper);
 	});
-
+/*
 	it('Rander guesses', ()=>{
 		const guessArray = [1,2,3];
 		const wrapper = shallow(<GuessList guesses={guessArray}/>);
 		expect(wrapper.exists('li')).toBe(true);
 	});
-
-	it('Rander 3 guesses', ()=>{
+*/
+	it('Rander 3 guesses correctly', ()=>{
 		const guessArray = [1,2,3];
 		const wrapper = shallow(<GuessList guesses={guessArray}/>);
 		expect(wrapper.find('li')).toHaveLength(3);
+		
+		/*
+		it('first number is 1',()=>{
+			expect(wrapper.find('ul').childAt(0).text("1")).toBeTruthy();
+		});
+		*/
+		
+		expect(wrapper.find('ul').childAt(0).text()).toEqual(guessArray[0].toString());
+		expect(wrapper.find('ul').childAt(1).text()).toEqual(guessArray[1].toString());
+		expect(wrapper.find('ul').childAt(2).text()).toEqual(guessArray[2].toString());
+		
 	});
 });
